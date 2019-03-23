d1be7735c94949b692493c891e49fd1a814bbf41
medajaite <mohamed.ajaite@tersea.com>
2019-03-23 23:14:26 +0100

    create an apollo client &  connect it to react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
 const title = 'United remote frontend coding challenge';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <ApolloProvider client={client}>
+    <div>
+      {title}
+    </div>
+  </ApolloProvider>,
   document.getElementById('app')
 );
 
